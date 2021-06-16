2c2fc16dc71a350f45222359e5a4ef3415754250
israelo19 <57731260+israelo19@users.noreply.github.com>
2021-06-15 22:31:52 -0400

    Update Part1.py

--- Part1.py
+++ Part1.py
@@ -1,3 +1,11 @@
 def solve():
   print("solving part 1")
-  pass
+  with open("moduleWeights.txt", "r") as f:
+    weightsText = f.read()
+    module_int = weightsText.split("\n")
+    
+    total_fuel = 0
+    for module in module_int:
+      total_fuel += int((int(module)/3) - 2 )
+    print(total_fuel)
+pass
